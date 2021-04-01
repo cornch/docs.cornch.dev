814112b58801d7adfc41a64a6e6e027f9927e3d0
Cornch <cornch@cornch.dev>
2021-04-01 12:07:43 +0800

    style: formatting

--- app/View/Components/DocSidebar.php
+++ app/View/Components/DocSidebar.php
@@ -3,34 +3,21 @@
 namespace App\View\Components;
 
 use App\DocLoader;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\Component;
 
-class DocSidebar extends Component
+final class DocSidebar extends Component
 {
-    /**
-     * Create a new component instance.
-     *
-     * @param \App\DocLoader $docLoader
-     * @param string|null $locale
-     * @param string|null $doc
-     * @param string|null $version
-     */
     public function __construct(
         public string $locale,
         public string $doc,
         public string $version,
         public string $page
     ) {
-
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
-    public function render()
+    public function render(): View
     {
         $docLoader = app(DocLoader::class, [
             'locale' => $this->locale,
