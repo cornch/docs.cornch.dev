92a7d737c0bff0333f1a834d1ea2598fe6b011d0
Cornch <cornch@cornch.dev>
2021-04-07 03:28:17 +0800

    fix: version switch

--- resources/views/docs/headers/laravel.blade.php
+++ resources/views/docs/headers/laravel.blade.php
@@ -5,8 +5,8 @@
                 <x-logo.laravel class="h-16 -ml-4" />
             </div>
             {{-- use SINGLE QUOTE here for x-data as json_encode is using double quote for string --}}
-            <div class="flex justify-end" x-data='{ locale: "{{ $locale }}", locales: @json($locales) }'>
-                <div class="relative mx-2">
+            <div class="flex justify-end">
+                <div class="relative mx-2" x-data='{ locale: "{{ $locale }}", locales: @json($locales) }'>
                     <select
                         class="px-4 pr-8 py-2 border border-red-400 hover:border-red-300 text-gray-900 rounded appearance-none"
                         x-model="locale"
@@ -25,7 +25,7 @@
                         <svg class="w-4 h-4 text-gray-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
                     </span>
                 </div>
-                <div class="relative mx-2">
+                <div class="relative mx-2" x-data="{ version: '{{ $version }}' }">
                     <select
                         class="px-4 pr-8 py-2 border border-red-400 hover:border-red-300 text-gray-900 rounded appearance-none"
                         x-model="version"
