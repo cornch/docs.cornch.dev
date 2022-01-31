bc97b196c1ac11a07ff99598bdf9674116056d15
Cornch <cornch@cornch.dev>
2022-01-31 16:02:23 +0800

    fix: properly handle links with hash

--- resources/views/components/doc-sidebar.blade.php
+++ resources/views/components/doc-sidebar.blade.php
@@ -1,7 +1,13 @@
 <div
     x-data="{ opening: '' }"
     x-init="$el.querySelectorAll('a').forEach((link) => {
-        if (link.href.startsWith(window.location.href)) {
+        if (
+            link.pathname === window.location.pathname &&
+            (
+                (link.hash && link.hash === window.location.hash) ||
+                !link.hash
+            )
+        ) {
             link.parentNode.classList.add('active');
 
             let $li = link.parentNode;
