a1885531dddede71e327d9fe373cb4a703a27b02
Cornch <cornch@cornch.dev>
2022-10-30 15:04:35 +0800

    fix(comments): button class usage

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,15 +1,16 @@
 @props(['type' => 'button'])
 
 <button
-    {{ $attributes->merge([
-        'type' => $type,
-        'class' => [
-            'inline-block',
-            'px-4 py-2',
-            'rounded',
-            'border',
-            'transition-colors',
-            ...$themeStyles(),
-        ]
-    ]) }}
+    {{
+        $attributes
+            ->merge(['type' => $type])
+            ->class([
+                'inline-block',
+                'px-4 py-2',
+                'rounded',
+                'border',
+                'transition-colors',
+                ...$themeStyles(),
+            ])
+    }}
 >{{ $slot }}</button>
